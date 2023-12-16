c81d667703306849b9de3895be435d96c1c093bf
Marcelo Carvalho <silva.marcelocarvalho@gmail.com>
2023-12-16 14:01:48 -0300

    added log track

--- auth/index.js
+++ auth/index.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const routes = require('./routes');
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
+
 require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
+
+if(process.env.NODE_ENV === 'dev') {
+    app.use(morgan("combined"));
+} else {    
+    app.use(morgan("tiny"));
+
+    const logFileStream = fs.createWriteStream(path.join(__dirname, 'app.log'), {flags: 'a'});
+    app.use(morgan("combined", {
+        stream: logFileStream
+    }));
+
+}
+
 app.use(routes);
 
 const port = process.env.PORT || 3001;
--- petstore/index.js
+++ petstore/index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require(`mongoose`);
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
 const DB = require(`./src/database/config`);
 require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
+
+if(process.env.NODE_ENV === 'dev') {
+    app.use(morgan("combined"));
+} else {    
+    app.use(morgan("tiny"));
+
+    const logFileStream = fs.createWriteStream(path.join(__dirname, 'app.log'), {flags: 'a'});
+    app.use(morgan("combined", {
+        stream: logFileStream
+    }));
+
+}
+
 app.use(routes);
 
 const port = process.env.PORT || 3000;
